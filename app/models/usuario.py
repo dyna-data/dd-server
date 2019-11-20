83c8be40e2f970d022f74d3eb924308b7a3e7e3f
Guilherme Leal <lealhugui@gmail.com>
2019-11-20 11:44:21 -0300

    nova base model

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,9 +1,15 @@
 from app.shared import db
+from .base import BaseModel
 
-class Usuario(db.Model):
+class Usuario(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    _default_fields = [
+        "username",
+        "email",
+    ]
 
     def __repr__(self):
         return '<User %r>' % self.username
